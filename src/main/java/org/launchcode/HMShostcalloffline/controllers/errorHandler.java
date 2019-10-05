5f62af78a7eba8ec9976677af69dee792620b640
Michael Sisay <ashenafimichael25@gmail.com>
2019-10-04 19:28:58 -0500

    added some styles to login page

---
+++ src/main/java/org/launchcode/HMShostcalloffline/controllers/errorHandler.java
@@ -0,0 +1,14 @@
+//package org.launchcode.HMShostcalloffline.controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//@Controller
+//public class errorHandler {
+//@RequestMapping(value="/error")
+//@ResponseBody
+//public String error(){
+//    return "redirect:/login/welcome";
+//}
+//}
